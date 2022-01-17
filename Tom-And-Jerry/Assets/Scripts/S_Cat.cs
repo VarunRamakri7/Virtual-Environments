9f6eed8e99b4318993d77cf77ca9646a89699ac3
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-01-17 17:33:00 -0500

    Added Cat AI
    - Move toward nearest mouse

--- Tom-And-Jerry/Assets/Scripts/S_Cat.cs
+++ Tom-And-Jerry/Assets/Scripts/S_Cat.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class S_Cat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject mouse;
+    [SerializeField]
+    private GameObject decoy;
 
-    // Update is called once per frame
-    void Update()
+    private float speed = 0.4f;
+
+    void FixedUpdate()
     {
-        
+        // Calculate distance between cat and mice
+        float distToMouse = Vector3.Distance(this.gameObject.transform.position, mouse.transform.position);
+        float distToDecoy = Vector3.Distance(this.gameObject.transform.position, decoy.transform.position);
+
+        GameObject obj = (distToDecoy <= distToMouse) ? decoy : mouse; // Get closest mouse
+
+        // Move cat towards  closest mouse
+        Vector3 dir = obj.transform.position - this.gameObject.transform.position; // Calculate direction vector
+        dir = dir.normalized; // Normalize resultant vector to unit Vector 
+        this.gameObject.transform.position += dir * Time.deltaTime * speed; // Move in the direction of the direction vector every frame
     }
 }
