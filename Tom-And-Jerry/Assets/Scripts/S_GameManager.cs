8b161c842ba853db2180d4411f63977cb35eecd0
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-01-17 16:53:38 -0500

    Basic booleans added

--- Tom-And-Jerry/Assets/Scripts/S_GameManager.cs
+++ Tom-And-Jerry/Assets/Scripts/S_GameManager.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class S_GameManager : MonoBehaviour
 {
+    public static S_GameManager gameManager;
+
+    private bool mouseCaught { get; set; }
+    private bool cheeseFound { get; set; }
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
