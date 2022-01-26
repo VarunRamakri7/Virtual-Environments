6be8d7231b66243314782d881c1494b89293de49
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-01-26 14:20:03 -0500

    Added script
    - PUN Manager

---
+++ Tom-And-Jerry/Assets/Scripts/S_PUNManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_PUNManager : MonoBehaviour
+{
+    public static S_PUNManager punManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (punManager == null)
+        {
+            punManager = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
