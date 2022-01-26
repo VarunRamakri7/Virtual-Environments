d4f490896fbcbb6b39e922b4019563818be60a64
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-01-26 14:39:59 -0500

    Added basic code

--- Tom-And-Jerry/Assets/Scripts/S_PUNManager.cs
+++ Tom-And-Jerry/Assets/Scripts/S_PUNManager.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class S_PUNManager : MonoBehaviour
+public class S_PUNManager : MonoBehaviourPunCallbacks
 {
     public static S_PUNManager punManager;
+
+    private string gameVersion = "1";
+
+    void Awake()
+    {
+        // #Critical
+        // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -13,11 +24,51 @@ public class S_PUNManager : MonoBehaviour
         {
             punManager = this;
         }
+
+        Connect();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// - If already connected, we attempt joining a random room
+    /// - if not yet connected, Connect this application instance to Photon Cloud Network
+    public void Connect()
     {
-        
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
     }
+
+#region MonoBehaviourPunCallbacks Callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN");
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom() called by PUN.");
+    }
+
+#endregion
 }
